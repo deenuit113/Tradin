f62fb9bd27cb6a8273cf335dab40f4045a692eb7
deenuit113 <leejh5314@gmail.com>
2024-07-16 13:42:36 +0900

    Fix: fix recoil duplicate key recoil 환경변수 설정 atom key에 난수 적용

--- coin/src/components/commons/atoms.ts
+++ coin/src/components/commons/atoms.ts
@@ -1,6 +1,10 @@
-import { atom } from 'recoil';
+import { atom,RecoilEnv } from 'recoil';
+import { v1 } from 'uuid';
+
+
+RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
 
 export const darkMode = atom({
-    key: 'darkMode',
+    key: `darkMode/${v1()}`,
     default: false,
 });
