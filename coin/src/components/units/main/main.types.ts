ee9c8fc3d376218716486976ae84b86532561974
deenuit113 <leejh5314@gmail.com>
2024-08-20 15:55:24 +0900

    Refactor: refactor main page container, presenter, types (interface) 파일로 분리)

---
+++ coin/src/components/units/main/main.types.ts
@@ -0,0 +1,14 @@
+export interface IMainPageUIProps {
+    widgets: { id: string; type: string; name: string }[]
+    removeWidget: (index: number) => void;
+    menuOpen: number | null;
+    setMenuOpen: React.Dispatch<React.SetStateAction<number | null>>;
+    moveWidget: (dragIndex: number, hoverIndex: number) => void;
+    setSelectedSymbol: React.Dispatch<React.SetStateAction<string | null>>;
+    onClickWidgetSelector: () => void;
+    addWidget: (widgetType: string) => void;
+    setWidgetSelectorOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    availableWidgetTypes: { type: string; name: string, icon: JSX.Element, symbol: string; }[]
+    widgetSelectorOpen: boolean;
+    selectedSymbol: string | null;
+}
