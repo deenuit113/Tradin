8ed91910a2bac227a3e833a624fc8ac3e4a59d04
deenuit113 <leejh5314@gmail.com>
2024-09-07 18:20:10 +0900

    :memo:Chore: modify firebase config 파이어베이스 설정 변경

--- coin/src/components/commons/util/firebase.ts
+++ coin/src/components/commons/util/firebase.ts
@@ -1,11 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, isSupported } from "firebase/analytics"; // isSupported 추가
+import { useEffect } from "react";
 
 // Firebase 설정
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain:  process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
@@ -15,10 +16,18 @@ const firebaseConfig = {
 
 // Firebase 초기화
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 // Firebase Authentication 초기화
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
+
+// 클라이언트 측에서만 Firebase Analytics 초기화
+if (typeof window !== "undefined") {
+  isSupported().then((supported) => {
+    if (supported) {
+      getAnalytics(app);
+    }
+  });
+}
 
 export { auth, googleProvider };
