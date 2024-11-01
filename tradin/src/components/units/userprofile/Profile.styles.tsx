e37f888de927a3804f0512889eed85be34536448
deenuit113 <leejh5314@gmail.com>
2024-11-01 17:28:48 +0900

    :zap:Feat: modify user profile page 프로필 화면 구성

--- tradin/src/components/units/userprofile/Profile.styles.tsx
+++ tradin/src/components/units/userprofile/Profile.styles.tsx
@@ -35,9 +35,10 @@ export const ProfileHeader = styled.div<{ sidebarOpen: boolean }>`
 export const MainContent = styled.div<{ sidebarOpen: boolean }>`
     width: ${({ sidebarOpen }) => (sidebarOpen ? "85%" : "100%")};
     max-width: 100%;
-    display: grid;
-    grid-template-columns: minmax(0, 1fr);
-    gap: 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
     margin-left: ${({ sidebarOpen }) => (sidebarOpen ? "15%" : "0")};
     transition: width 0.3s ease, margin-left 0.3s ease;
     height: 100%;
@@ -45,6 +46,7 @@ export const MainContent = styled.div<{ sidebarOpen: boolean }>`
     overflow-x: hidden;
     padding: 2rem;
     box-sizing: border-box;
+    gap: 20px;
 
     &::-webkit-scrollbar {
         width: 12px;
@@ -75,3 +77,79 @@ export const MainContent = styled.div<{ sidebarOpen: boolean }>`
         padding: 0.5rem;
     }
 `;
+
+export const UserImg = styled.img`
+    width: 100px;
+    height: 100px;
+    object-fit: cover;
+    border: 2px solid ${({ theme }) => theme.borderColor};
+    border-radius: 50%;
+
+    @media (max-width: 799px) {
+        width: 75px;
+        height: 75px;
+    }
+`
+
+export const SignOutButton = styled.button`
+    background-color: transparent;
+    border-radius: 8px;
+    font-weight: 700;
+    border: 1px solid ${({ theme }) => theme.borderColor};
+    color: ${({ theme }) => theme.textColor};
+    cursor: pointer;
+    padding: 0.5rem 1rem;
+
+    @media (max-width: 799px) {
+        padding: 0.3rem 0.6rem;
+    }
+`
+
+export const UserInfoContainer = styled.div`
+    width: 15%;
+    display: flex;
+    flex-direction: row;
+    align-items: flex start;
+    justify-content: flex-start;
+    
+    @media (max-width: 799px) {
+        width: 30%;
+    }
+`
+
+export const UserInfoTitleContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    font-weight: 700;
+    flex-grow: 1;
+`
+
+export const UserInfoTitle = styled.label`
+    width: 100%;
+    height: 100%;
+    padding: 5px 10px;
+    color: ${({ theme }) => theme.textColor};
+    justify-content: flex-start;
+    align-items: flex-start;
+    font-size: 1rem;
+`
+export const UserInfoDataContainer = styled.div`
+    width: 70%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+`
+
+export const UserInfoData = styled.label`
+    width: 100%;
+    height: 100%;
+    padding: 5px 10px;
+    color: ${({ theme }) => theme.textColor};
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+    font-size: 0.8rem;
+`
