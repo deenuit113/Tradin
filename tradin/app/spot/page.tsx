bcb99077239b51ecad3cb6648d138134d0050c50
deenuit113 <leejh5314@gmail.com>
2024-11-03 22:20:42 +0900

    :pencil2:Fix: fix spot page import path 선물 페이지 컴포넌트 경로 변경

--- tradin/app/spot/page.tsx
+++ tradin/app/spot/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import SpotPage from "../../src/components/units/strategy/spot/Spot";
+import SpotPage from "../../src/components/units/strategy/spot/Spot.container";
 
 export default function Spot() {
     return (
