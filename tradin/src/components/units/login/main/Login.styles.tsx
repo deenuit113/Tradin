612c9ec199f93071b9f79314501de70cb53f65dc
deenuit113 <leejh5314@gmail.com>
2024-11-04 22:32:52 +0900

    :lipstick:Design: modify login page UI 로그인 폼 너비 변경

--- tradin/src/components/units/login/main/Login.styles.tsx
+++ tradin/src/components/units/login/main/Login.styles.tsx
@@ -34,7 +34,7 @@ export const LoginForm = styled.form`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    width: 40%;
+    width: 300px;
     margin-bottom: 1rem;
 `
 
