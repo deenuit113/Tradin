3cc9e41695bc8c4e256cb30859f6ad84bda74716
deenuit113 <leejh5314@gmail.com>
2024-11-22 15:12:33 +0900

    :building_construction:Design: migrate breadcrumb 브레드크럼 차크라로 마이그레이션 전략페이지 이름 수정 기능 제거 Related to: #83

--- tradin/src/components/commons/breadcrumb/BreadCrumb.presenter.tsx
+++ tradin/src/components/commons/breadcrumb/BreadCrumb.presenter.tsx
@@ -1,55 +1,66 @@
 import React from 'react';
 import { getPathLabel } from './PageStructure';
 import { FaHome } from 'react-icons/fa';
-import * as S from "./BreadCrumb.styles";
 import { BreadCrumbUIProps } from './BreadCrumb.types';
-
+import { 
+    BreadcrumbCurrentLink, 
+    BreadcrumbLink, 
+    BreadcrumbRoot 
+} from '@/components/ui/breadcrumb';
+import {
+    MenuContent,
+    MenuItem,
+    MenuRoot,
+    MenuTrigger,
+} from '@/components/ui/menu';
+import { LuChevronDown } from 'react-icons/lu';
 export default function BreadcrumbUI(props: BreadCrumbUIProps): JSX.Element {
+    // mock
+    const mockData = [
+        { name: '1', id: '1' },
+        { name: '2', id: '2' },
+        { name: '3', id: '3' },
+    ];
 
     return (
-        <S.BreadcrumbContainer aria-label="breadcrumb">
-            <S.BreadcrumbList>
-                <S.BreadcrumbItem>
-                    <S.BreadcrumbLink href="/">
-                        <FaHome style={{ marginRight: '5px' }} />
-                        메인
-                    </S.BreadcrumbLink>
-                </S.BreadcrumbItem>
-                {props.pathSegments.map((segment, index) => {
-                    const url = `/${props.pathSegments.slice(0, index + 1).join('/')}`;
-                    const isLast = index === props.pathSegments.length - 1;
-                    const isEditable = props.isEditablePath(props.pathSegments.slice(0, index + 1));
-                    const label = getPathLabel(url);
+        <BreadcrumbRoot aria-label="breadcrumb">
+            <BreadcrumbLink href="/">
+                <FaHome style={{ marginRight: '5px' }} />
+                메인
+            </BreadcrumbLink>
 
-                    return (
-                        <React.Fragment key={url}>
-                            <S.Separator />
-                            <S.BreadcrumbItem>
-                                {props.editingSegment === segment ? (
-                                    <S.EditInput
-                                        value={props.editValue}
-                                        onChange={(e) => props.setEditValue(e.target.value)}
-                                        onBlur={() => props.handleSave(index)}
-                                        onKeyPress={(e) => e.key === 'Enter' && props.handleSave(index)}
-                                        autoFocus
-                                    />
-                                ) : (
-                                    <>
-                                        {isLast ? (
-                                            <span>{label}</span>
-                                        ) : (
-                                            <S.BreadcrumbLink href={url}>{label}</S.BreadcrumbLink>
-                                        )}
-                                        {isEditable && (
-                                            <S.EditIcon onClick={() => props.handleEdit(segment, index)} />
-                                        )}
-                                    </>
-                                )}
-                            </S.BreadcrumbItem>
-                        </React.Fragment>
-                    );
-                })}
-            </S.BreadcrumbList>
-        </S.BreadcrumbContainer>
+            {props.pathSegments.map((segment, index) => {
+                const url = `/${props.pathSegments.slice(0, index + 1).join('/')}`;
+                const isLast = index === props.pathSegments.length - 1;
+                const label = getPathLabel(url);
+
+                const isDropdown = segment === 'spot' || segment === 'future';
+
+                return (
+                    <React.Fragment key={url}>
+                        {isLast ? (
+                            <BreadcrumbCurrentLink>{label}</BreadcrumbCurrentLink>
+                        ) : isDropdown ? (
+                            <MenuRoot> 
+                                <MenuTrigger asChild>
+                                    <BreadcrumbLink as="button">
+                                        {label} <LuChevronDown />
+                                    </BreadcrumbLink>
+                                </MenuTrigger>
+                                <MenuContent bg="backgroundColor">
+                                    {mockData.map((item) => (
+                                        <MenuItem key={item.id} value={item.id} asChild>
+                                            <a href={`/${segment}/${item.id}`}>{item.name}</a>
+                                        </MenuItem>
+                                    ))}
+                                </MenuContent>
+                            </MenuRoot>
+                        ) : (
+                            <BreadcrumbLink href={url}>{label}</BreadcrumbLink>
+                        )}
+                    </React.Fragment>
+                );
+            })}
+        </BreadcrumbRoot>
     );
-};
+}
--- tradin/src/components/commons/breadcrumb/BreadCrumb.types.ts
+++ tradin/src/components/commons/breadcrumb/BreadCrumb.types.ts
@@ -1,9 +1,3 @@
 export interface BreadCrumbUIProps {
     pathSegments: string[]
-    handleSave: (index: number) => void
-    handleEdit: (segment: string, index: number) => void
-    editingSegment: string | null
-    editValue: string
-    isEditablePath: (path: string[]) => boolean
-    setEditValue: React.Dispatch<React.SetStateAction<string>>
 }
